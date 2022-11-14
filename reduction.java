b98bfafacfa357bf89d5c29156c536a9d31c1acf
Aaron Djangmah <96618832+zezecodes@users.noreply.github.com>
2022-11-14 12:30:12 +0000

    Delete reduction.java

--- reduction.java
+++
@@ -1,8 +0,0 @@
-public class reduction {
-    i=bottleInputNumber;
-    while (i<userReminderInput && i<=bottleInputNumber){
-        System.out.println(i);
-        i-=1;
-    }
-    System.out.println("Time to refill")
-}
