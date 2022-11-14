608b45470d6e9db39d9091bff18b50326dce3249
Aaron Djangmah <96618832+zezecodes@users.noreply.github.com>
2022-11-14 12:28:59 +0000

    updates

--- src/main/java/WaterReminderSystem.java
+++ src/main/java/WaterReminderSystem.java
@@ -6,10 +6,15 @@ public class WaterReminderSystem {
         System.out.println("Welcome to the water reminder system!\n------------------------------------");
 
         // List of healthy water facts that change everytime user launches program
-        String[] waterQuotesList = new String[] {"Adult humans are 60 percent water, and our blood is 90 percent water", "The average amount of water you need per day is about 3 liters (13 cups) for men and 2.2 liters (9 cups) for women",
-                "Water is essential for the kidneys and other bodily functions", "When dehydrated, the skin can become more vulnerable to skin disorders and wrinkling",
-                "Drinking water instead of soda can help with weight loss", "Around 71 percent of the planet’s surface is covered by water", "Drinking water cushions the brain, spinal cord, and other sensitive tissues",
+        String[] waterQuotesList = new String[] {"Adult humans are 60 percent water, and our blood is 90 percent water",
+                "The average amount of water you need per day is about 3 liters (13 cups) for men and 2.2 liters (9 cups) for women",
+                "Water is essential for the kidneys and other bodily functions",
+                "When dehydrated, the skin can become more vulnerable to skin disorders and wrinkling",
+                "Drinking water instead of soda can help with weight loss",
+                "Around 71 percent of the planet’s surface is covered by water",
+                "Drinking water cushions the brain, spinal cord, and other sensitive tissues",
                 "Approximately 80'%' of your brain tissue is made of water"};
+
         // initializing variables for inputs
         int bottleInputNumber, bottleRefillNumber, bottleTakeoutNumber ,bottleReminderNumber ;
 
@@ -35,13 +40,27 @@ public class WaterReminderSystem {
         System.out.print("How many bottles should be in the fridge for reminder to begin? - ");
         bottleReminderNumber = bottleInput.nextInt();
 
+        // Shows the total amount of bottles that will be in the fridge based on the info given
+        int totalBottleEstimation = bottleInputNumber * bottleRefillNumber;
+        System.out.println("\nCalculating estimated total number of bottles in your fridge...");
+        if (totalBottleEstimation >= bottleTakeoutNumber){
+            System.out.println("Estimated number of bottles is: "+ totalBottleEstimation + " bottles \n");
+        }else {
+            System.out.println("Estimation cannot occur because number of bottles to be taken out is more than " +
+                    "number of bottles put in the fridge. Please try again \n");
+        }
+
         // Bottle reduction visualization logic (More accurate timing and implementation will be added)
         int i=bottleInputNumber;
+        System.out.println("""
+                Initializing reminder...
+                You will be prompted for a refill when bottle number has been reached.
+                """);
         while(i>bottleReminderNumber && i<=bottleInputNumber){
             System.out.println(i);
             i-=1;
         }
-        System.out.println("Time to refill");
+        System.out.println(bottleReminderNumber + "\nTime to refill");
 
 
 
